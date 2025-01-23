fc4e34dddee943b60ef4ef317ce351c5b7e064d3
wxiaoyun <w.xy020203@gmail.com>
2025-01-23 15:42:27 +0800

    misc: initialize project

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import pluginJs from "@eslint/js";
+import * as tsParser from "@typescript-eslint/parser";
+import solid from "eslint-plugin-solid/configs/typescript";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ["node_modules", "dist", "build", "src-tauri", "**/*.config.{ts,js}" ],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...solid,
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+    },
+  },
+];
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <main class="container">
+    </main>
+  );
+}
+
+export default App;
