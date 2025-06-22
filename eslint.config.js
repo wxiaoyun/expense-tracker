65d98bc4f116807575435c063f5ae40809f9114a
wxiaoyun <w.xy020203@gmail.com>
2025-06-22 20:56:39 +0800

    revert: react compiler

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
-const reactCompiler = require("eslint-plugin-react-compiler");
 const expoConfig = require("eslint-config-expo/flat");
 const pluginQuery = require("@tanstack/eslint-plugin-query");
 
@@ -9,6 +8,5 @@ module.exports = defineConfig([
   {
     ignores: ["dist/*"],
   },
-  reactCompiler.configs.recommended,
   ...pluginQuery.configs["flat/recommended"],
 ]);
