c076c494b195a1a13887d1847d3f4ce8b172bd37
wxiaoyun <w.xy020203@gmail.com>
2026-08-22 19:50:20 +0800

    fix: app icon and theme issue

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -1,13 +1,9 @@
-import { useColorScheme } from 'react-native';
-
 export const useThemeColor = (key: 'text' | 'background' | 'backgroundSecondary' | 'primary') => {
-  const scheme = useColorScheme();
-  const isDark = scheme === 'dark';
   const colors = {
-    text: isDark ? '#fff' : '#000',
-    background: isDark ? '#000' : '#fff',
-    backgroundSecondary: isDark ? '#1c1c1e' : '#f2f2f7',
-    primary: isDark ? '#0a84ff' : '#007aff',
+    text: '#000',
+    background: '#fff',
+    backgroundSecondary: '#f2f2f7',
+    primary: '#007aff',
   };
   return colors[key];
 };
